f912cbacb6ccc542d4a01af0995a6a734deb9da0
Leandro Bugnon <leandro@adahome.com>
2016-11-03 02:48:32 -0300

    Agrego entrenamiento y test con generadores

--- base_convnet/base_convnet.py
+++ base_convnet/base_convnet.py
@@ -33,9 +33,13 @@ trn_path = "../dataset/data_dest_dir/train/chars/"
 val_path = "../dataset/data_dest_dir/validation/chars/"
 tst_path = "../dataset/data_dest_dir/test/chars/"
 
+# En este generador metemos el aumentado.
+# TODO: Ojo que el generador de Train solo tiene que tener aumentado (Hay que hacer un generator para train, y un generator pelado para valid/test)
 img_generator = ImageDataGenerator(
                     rescale=1./255
                     )
+
+# Viendo que hay dos formas que nadie se pone de acuerdo, tomo este orden que pusiste acá: validación para elegir hiperparámetros, test para el test final.
 
 trn_dataset = img_generator.flow_from_directory(
     trn_path,
@@ -50,29 +54,39 @@ tst_dataset = img_generator.flow_from_directory(
     batch_size=128
 )
 
-# the data, shuffled and split between train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+trainSamples=trn_dataset.N
+validationSamples=val_dataset.N
+testSamples=tst_dataset.N
 
-if K.image_dim_ordering() == 'th':
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
 
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
+# TODO: hay que resolver el tema de las clases. Creo que por defecto estaría en clases categórcas.
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+# Desde aca es mnist: the data, shuffled and split between train and test sets
+# (X_train, y_train), (X_test, y_test) = mnist.load_data() # TODO: esto es del mnist no? Yo digo que hagamos todo usando generators que despues vamos a neceistar... 
+
+# if K.image_dim_ordering() == 'th':
+#     X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+#     X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+#     input_shape = (1, img_rows, img_cols)
+# else:
+#     X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+#     X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+#     input_shape = (img_rows, img_cols, 1)
+
+# X_train = X_train.astype('float32')
+# X_test = X_test.astype('float32')
+# X_train /= 255
+# X_test /= 255
+# print('X_train shape:', X_train.shape)
+# print(X_train.shape[0], 'train samples')
+# print(X_test.shape[0], 'test samples')
+
+# # convert class vectors to binary class matrices
+# Y_train = np_utils.to_categorical(y_train, nb_classes)
+# Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+# Hasta aca parece que es todo mnist
 
 model = Sequential()
 
@@ -93,11 +107,22 @@ model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
+              optimizer='adadelta', # alternativa: rmsprop?
               metrics=['accuracy'])
 
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1, validation_data=(X_test, Y_test))
-score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+history=model.fit_generator(trn_dataset, batch_size=batch_size, nb_epoch=nb_epoch,
+          verbose=1) # nunca me termino de convencer esta implemntación...Entiendo que si no vamos a hacer algo como early stop, no necesitamos pasar validación acá. .
+trainAcc=history.history['acc'][-1]
+                    
+
+# Error de validación: para optimizar hiperparámetros
+scoreV = model.evaluate(val_dataset,validationSamples, verbose=0)
+print('Valid score:', scoreV[0])
+print('Valid accuracy:', scoreV[1])
+
+
+# Aca hay que elegir los mejores hiperparametros, reentrenar con train+validación (hay que hacer otro generador...no se si hace falta dado que son muchos datos), y despues se evalua en test
+
+scoreV = model.evaluate_generator(tst_dataset,testSamples, verbose=0)
+print('Test score:', scoreT[0])
+print('Test accuracy:', scoreT[1])
