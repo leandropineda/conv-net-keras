7ad15ac404d1646506d7249a36799e14e6ca4a5d
Leandro Pineda <leandropineda.lp@gmail.com>
2016-11-05 16:17:12 -0300

    Experimento base

--- base_convnet/base_convnet.py
+++ base_convnet/base_convnet.py
@@ -3,19 +3,21 @@
 
 from __future__ import print_function
 import numpy as np
-np.random.seed(1337)  # for reproducibility
+#np.random.seed(1337)  # for reproducibility
 
-from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import json
+import pprint
 
 batch_size = 32
 nb_classes = 91
-nb_epoch = 12
+nb_epoch = 15
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -26,9 +28,12 @@ pool_size = (2, 2)
 # convolution kernel size
 kernel_size = (3, 3)
 
-# dataset path
+####################################################################################################################
+# Dataset path
+####################################################################################################################
 # It should contain one subdirectory per class, and 
 # the subdirectories should contain PNG or JPG images.
+####################################################################################################################
 trn_path = "../dataset/data_dest_dir/train/chars/"
 val_path = "../dataset/data_dest_dir/validation/chars/"
 tst_path = "../dataset/data_dest_dir/test/chars/"
@@ -36,31 +41,31 @@ tst_path = "../dataset/data_dest_dir/test/chars/"
 # En este generador metemos el aumentado.
 # TODO: Ojo que el generador de Train solo tiene que tener aumentado (Hay que hacer un generator para train, y un generator pelado para valid/test)
 img_generator = ImageDataGenerator(
-                    rescale=1./255,
-                    samplewise_center=True,
-                    samplewise_std_normalization=True
+                    #rescale=1./255,
+                    #samplewise_center=True,
+                    #samplewise_std_normalization=True
                     )
 
 # Viendo que hay dos formas que nadie se pone de acuerdo, tomo este orden que pusiste acá: validación para elegir hiperparámetros, test para el test final.
 
 trn_dataset = img_generator.flow_from_directory(
     trn_path,
-    target_size=(img_rows,img_cols),
-    color_mode='grayscale',
+    target_size=(img_rows, img_cols),
+    #color_mode='grayscale',
     class_mode='categorical',
     batch_size=batch_size
 )
 val_dataset = img_generator.flow_from_directory(
     val_path,
-    taget_size=(img_rows,img_cols),
-    color_mode='grayscale',
+    target_size=(img_rows, img_cols),
+    #color_mode='grayscale',
     class_mode='categorical',
     batch_size=batch_size
 )
 tst_dataset = img_generator.flow_from_directory(
     tst_path,
-    target_size=(img_rows,img_cols),
-    color_mode='grayscale',
+    target_size=(img_rows, img_cols),
+    #color_mode='grayscale',
     class_mode='categorical',
     batch_size=batch_size
 )
@@ -70,7 +75,11 @@ validationSamples = val_dataset.N
 testSamples = tst_dataset.N
 
 input_shape = trn_dataset.image_shape
+print("Image shape: " + str(input_shape))
 
+####################################################################################################################
+# Build the model
+####################################################################################################################
 model = Sequential()
 
 model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
@@ -90,22 +99,65 @@ model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta', # alternativa: rmsprop?
+              optimizer='adadelta',  # alternativa: rmsprop?
               metrics=['accuracy'])
 
-history = model.fit_generator(trn_dataset, 
+model.summary()
+
+####################################################################################################################
+# Save the model to a json file
+####################################################################################################################
+text_file = open('model.json', 'w')
+model_json = str(model.to_json())
+parsed_model_json = json.loads(model_json)
+text_file.write(json.dumps(parsed_model_json,  # JSON pretty print
+                           sort_keys=True,
+                           indent=4))
+text_file.close()
+
+####################################################################################################################
+# Start training
+####################################################################################################################
+history = model.fit_generator(trn_dataset,
                                 nb_epoch=nb_epoch,
                                 samples_per_epoch=trainSamples,
                                 validation_data=val_dataset,
-                                nb_val_samples=validationSamples,
-                                verbose=1) # nunca me termino de convencer esta implemntación...Entiendo que si no vamos a hacer algo como early stop, no necesitamos pasar validación acá. .
-trainAcc = history.history['acc'][-1]
-
+                                nb_val_samples=validationSamples)
 
 # Aca hay que elegir los mejores hiperparametros, reentrenar con train+validación (hay que hacer otro generador...no se si hace falta dado que son muchos datos), y despues se evalua en test
 
-scoreT = model.evaluate_generator(tst_dataset, testSamples, verbose=0)
-print('Test score:', scoreT[0])
-print('Test accuracy:', scoreT[1])
+scoreT = model.evaluate_generator(tst_dataset, testSamples)
 
-print(history.history)
+####################################################################################################################
+# Save some results to a text file
+####################################################################################################################
+text_file = open('results.txt', 'w')
+text_file.write('Test score: ' + str(scoreT[0]))
+text_file.write('\nTest accuracy: ' + str(scoreT[1]))
+text_file.write('\n\nNumber of parameters: ' + str(model.count_params()))
+text_file.write('\n\nImage shape: ' + str(input_shape))
+text_file.write('\n\n')
+pp = pprint.PrettyPrinter(indent=4)  # dictionary pretty print
+text_file.write(pp.pformat(history.history))
+text_file.close()
+
+####################################################################################################################
+# Plot some results and save to file
+####################################################################################################################
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.savefig("results_acc.png")
+# summarize history for loss
+plt.clf()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.savefig("results_loss.png")
