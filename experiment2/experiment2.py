3f0802d385a314d16f8855b4bbca120f38f73d81
Leandro Pineda <leandropineda.lp@gmail.com>
2016-11-16 13:42:50 -0300

    Experimento con mas capas

--- experiment2/experiment2.py
+++ experiment2/experiment2.py
@@ -12,13 +12,50 @@ from keras.layers.normalization import BatchNormalization
 from keras.utils import np_utils
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
 import matplotlib.pyplot as plt
 import json
 import pprint
+import numpy as np
+import itertools
+from sklearn.metrics import confusion_matrix
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+
+
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = 50.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="red" if cm[i, j] > thresh and i != j else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 batch_size = 32
 nb_classes = 91
-nb_epoch = 30
+nb_epoch = 40
 
 # input image dimensions
 img_rows, img_cols = 32, 32
@@ -87,35 +124,46 @@ model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
                         border_mode='same',
                         input_shape=input_shape))
 model.add(Activation('relu'))
+model.add(BatchNormalization()) # delete this
 
-model.add(BatchNormalization())
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                        border_mode='same'))
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1], border_mode='same'))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1], border_mode='same'))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1], border_mode='same'))
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=pool_size))
+
+model.add(Convolution2D(nb_filters*2, kernel_size[0], kernel_size[1], border_mode='same'))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(nb_filters*2, kernel_size[0], kernel_size[1], border_mode='same'))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(nb_filters*4, kernel_size[0], kernel_size[1], border_mode='same'))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(nb_filters*4, kernel_size[0], kernel_size[1], border_mode='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=pool_size))
-model.add(SpatialDropout2D(0.5))
 
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-
-
-model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(400))
+model.add(Dense(2048, init='he_normal'))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1000))
+model.add(Dense(1024, init='he_normal'))
 model.add(Activation('relu'))
-model.add(Dense(nb_classes))
+model.add(Dropout(0.5))
+model.add(Dense(512, init='he_normal'))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(nb_classes, init='he_normal'))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
@@ -127,22 +175,44 @@ model.summary()
 ####################################################################################################################
 # Start training
 ####################################################################################################################
+print("Training Started...")
 history = model.fit_generator(trn_dataset,
                               nb_epoch=nb_epoch,
                               samples_per_epoch=trn_samples,
                               validation_data=val_dataset,
                               nb_val_samples=val_samples)
+model.save('model.hdf5')
 
+#model = load_model('model.h5')
 # Aca hay que elegir los mejores hiperparametros, reentrenar con train+validación (hay que hacer otro generador...no se si hace falta dado que son muchos datos), y despues se evalua en test
 
+print("Evaluating Model...")
 scoreT = model.evaluate_generator(tst_dataset, tst_samples)
 print("Test score: " + str(scoreT[0]) + "\nTest accuracy: " + str(scoreT[1]))
 
-#y_pred = model.predict_generator(generator=tst_dataset, val_samples=tst_samples)
+####################################################################################################################
+# Confusion matrix
+####################################################################################################################
+print("Building Confusion Matrix...")
+y_true = []
+y_pred = []
+i = 0
+while i < (int(tst_samples/batch_size) + 1):
+    batch = tst_dataset.next()
+    y_true.append(np.apply_along_axis(np.argmax, 1, batch[1]))
+    y_pred.append(np.apply_along_axis(np.argmax, 1, model.predict(batch[0], batch_size=batch_size)))
+    i += 1
 
-#y_pred = [np.argmax(x) for x in y_pred] # max pos
-#print(y_pred)
+y_true = [i for subl in y_true for i in subl]
+y_pred = [i for subl in y_pred for i in subl]
 
+classes = np.unique(np.concatenate((y_true, y_pred))) + 32
+
+cnf_matrix = confusion_matrix(y_true, y_pred)
+plt.figure(figsize=(80, 80))
+plot_confusion_matrix(cnf_matrix, classes=classes,
+                      title='Confusion matrix')
+plt.savefig('cnf_matrix.pdf', format='pdf')
 
 ####################################################################################################################
 # Save the model to a json file
@@ -172,6 +242,7 @@ text_file.close()
 # Plot some results and save to file
 ####################################################################################################################
 # summarize history for accuracy
+plt.clf()
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('model accuracy')
